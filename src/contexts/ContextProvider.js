e6211e02b5f09a75b62e044fc4c4da6a14c3b4a9
Priyanshu-Negi-141 <priyanshunegi121@gmail.com>
2023-08-01 11:49:08 +0530

    newUpdate

--- src/contexts/ContextProvider.js
+++ src/contexts/ContextProvider.js
@@ -28,7 +28,7 @@ export const ContextProvider = ({children}) => {
     const [selectedStream, setSelectedStream] = useState('')
     // Auth and fetching data context
     // const host = "http://localhost:8000"
-    const host = "http://3.6.89.177:8001" 
+    const host = "http://3.6.89.177:8001"
     
     const [isPopupOpen, setPopupOpen] = useState(false);
     const [isPopupCheckOutOpen, setPopupCheckOutOpen] = useState(false);
